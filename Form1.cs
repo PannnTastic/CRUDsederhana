1ca1064ecb5f5661e078f6105ed0b171336e2340
PannnTastic <pannntastic@gmail.com>
2025-03-21 01:44:43 +0800

    query insert

--- Form1.cs
+++ Form1.cs
@@ -71,6 +71,8 @@ namespace CRUDsederhana
                     }
 
                     conn.Open();
+                    string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
+
                 }
                 catch (Exception ex)
                 {
