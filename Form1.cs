eb7c6dc1888f564196f6d823c8e68ca89b5396e7
PannnTastic <pannntastic@gmail.com>
2025-03-21 01:52:24 +0800

    meload data

--- Form1.cs
+++ Form1.cs
@@ -84,7 +84,7 @@ namespace CRUDsederhana
                         if (rowsAffected > 0)
                         {
                             MessageBox.Show("Data berhasil ditambahkan", "Sukses" , MessageBoxButtons.OK , MessageBoxIcon.Information);
-                            
+                            LoadData();
                         }
                     }
                 }
